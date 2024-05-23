6a3e2ff8424c828c0ac7b572e6d7af189407ff47
Sridhar Ratnakumar <srid@srid.ca>
2024-05-23 17:43:19 -0400

    chore: treat inputs as non-flakes
    This avoids flake.lock explosion, at the small cost of having to
    manually defining the template (with path in the repo).

--- flake.nix
+++ flake.nix
@@ -5,11 +5,15 @@
     # Pull templates from external flake-parts modules
     # The pull happens in CI periodically.
     haskell-flake.url = "github:srid/haskell-flake";
+    haskell-flake.flake = false;
   };
   outputs = inputs: {
     templates = rec {
       haskell = haskell-flake;
-      haskell-flake = inputs.haskell-flake.templates.example;
+      haskell-flake = {
+        description = "Haskell project template, using haskell-flake";
+        path = builtins.path { path = inputs.haskell-flake + /example; };
+      };
     };
 
     # To make HCI not fail; remove after adding apps or checks.
