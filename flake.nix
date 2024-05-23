b6e0ea1f1ead845d3ec076acbc8ea2610c02df4c
Sridhar Ratnakumar <srid@srid.ca>
2024-05-23 17:40:47 -0400

    fix: add dummy drv for HCI not to fail
    Ref: https://hercules-ci.com/github/flake-parts/templates/jobs/4

--- flake.nix
+++ flake.nix
@@ -1,5 +1,7 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     # Pull templates from external flake-parts modules
     # The pull happens in CI periodically.
     haskell-flake.url = "github:srid/haskell-flake";
@@ -9,5 +11,8 @@
       haskell = haskell-flake;
       haskell-flake = inputs.haskell-flake.templates.example;
     };
+
+    # To make HCI not fail; remove after adding apps or checks.
+    packages.x86_64-linux.hello = inputs.nixpkgs.legacyPackages.x86_64-linux.hello;
   };
 }
