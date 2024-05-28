88741d04ab4ec42310fb6b8876e5d2e7d23ab70a
Sridhar Ratnakumar <srid@srid.ca>
2024-05-28 18:32:08 -0400

    wip

--- flake.nix
+++ flake.nix
@@ -13,10 +13,29 @@
       haskell-flake = {
         description = "Haskell project template, using haskell-flake";
         path = builtins.path { path = inputs.haskell-flake + /example; };
+        params = {
+          cabal-package-name.description = "Name of the Haskell package";
+          cabal-package-name.exec = val: ''
+            mv example.cabal ${val}.cabal
+            sed -i 's/example/${val}/g' ${val}.cabal 
+            sed -i 's/example/${val}/g' flake.nix
+          '';
+        };
       };
     };
 
     # To make HCI not fail; remove after adding apps or checks.
     packages.x86_64-linux.hello = inputs.nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.aarch64-darwin.default = inputs.nixpkgs.legacyPackages.aarch64-darwin.writeShellApplication {
+      name = "default";
+      text = ''
+        set -x
+        nix flake init -t ${inputs.self}#haskell
+        echo "# cabal-package-name"
+        ${inputs.self.templates.haskell.params.cabal-package-name.patch "my-haskell-project"}
+        echo "done!"
+      '';
+    };
   };
 }
