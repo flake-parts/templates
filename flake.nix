a37b6c3fd976a09d82d9869e30debda0dffcae29
Sridhar Ratnakumar <srid@srid.ca>
2024-06-01 19:09:07 -0400

    chore: Use flake-parts & treefmt (nixpkgs-fmt)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
 
     # Pull templates from external flake-parts modules
     # The pull happens in CI periodically.
@@ -8,18 +11,33 @@
     haskell-flake.flake = false;
     nix-dev-home.url = "github:juspay/nix-dev-home";
   };
-  outputs = inputs: {
-    templates = rec {
-      home-manager = nix-dev-home;
-      nix-dev-home = inputs.nix-dev-home.templates.default;
-      haskell = haskell-flake;
-      haskell-flake = {
-        description = "Haskell project template, using haskell-flake";
-        path = builtins.path { path = inputs.haskell-flake + /example; };
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem = {
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixpkgs-fmt.enable = true;
+          };
+        };
+      };
+      flake = {
+        templates = rec {
+          home-manager = nix-dev-home;
+          nix-dev-home = inputs.nix-dev-home.templates.default;
+          haskell = haskell-flake;
+          haskell-flake = {
+            description = "Haskell project template, using haskell-flake";
+            path = builtins.path { path = inputs.haskell-flake + /example; };
+          };
+        };
+
+        # To make HCI not fail; remove after adding apps or checks.
+        packages.x86_64-linux.hello = inputs.nixpkgs.legacyPackages.x86_64-linux.hello;
       };
     };
-
-    # To make HCI not fail; remove after adding apps or checks.
-    packages.x86_64-linux.hello = inputs.nixpkgs.legacyPackages.x86_64-linux.hello;
-  };
 }
