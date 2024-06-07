9bd516c09aae8f5ec8d95a3e066ac06d341e516b
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-06-07 10:51:34 -0400

    Enrich `nix flake init` with parameters (#5)

--- flake.nix
+++ flake.nix
@@ -28,11 +28,55 @@
       flake = {
         templates = rec {
           home-manager = nix-dev-home;
-          nix-dev-home = inputs.nix-dev-home.templates.default;
+          nix-dev-home = inputs.nix-dev-home.templates.default // {
+            # TODO: Ideally, these params should be moved to upstream module.
+            # But do that only as the spec stabliizes.
+            params = {
+              username = {
+                name = "Username";
+                help = "Your username as shown by by $USER";
+                default = "runner";
+                required = true;
+                files = [
+                  "flake.nix"
+                ];
+              };
+              full-name = {
+                name = "Full Name";
+                help = "Your full name for use in Git config";
+                default = "John Doe";
+                required = true;
+                files = [
+                  "home/default.nix"
+                ];
+              };
+              email = {
+                name = "Email";
+                help = "Your email for use in Git config";
+                default = "johndoe@example.com";
+                required = true;
+                files = [
+                  "home/default.nix"
+                ];
+              };
+            };
+          };
           haskell = haskell-flake;
           haskell-flake = {
             description = "Haskell project template, using haskell-flake";
             path = builtins.path { path = inputs.haskell-flake + /example; };
+            params = {
+              cabal-package-name = {
+                name = "Package Name";
+                help = "Name of the Haskell package";
+                default = "example";
+                required = false;
+                files = [
+                  "example.cabal"
+                  "flake.nix"
+                ];
+              };
+            };
           };
         };
         herculesCI.ciSystems = [ "x86_64-linux" ];
